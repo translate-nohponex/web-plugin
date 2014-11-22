f82294896b4c7d91e1ed34cfcea399e115a97d79
NohponeX <NohponeX@gmail.com>
2014-11-22 16:25:18 +0200

    Add i18n-data paramaters

--- js/translation.js
+++ js/translation.js
@@ -46,7 +46,7 @@ $(document).ready( function() {
 	var translate_page = function( root ){
 		root = typeof( root ) !== 'undefined' ? root : 'body';
 
-		data = $( root ).find("[data-i18]");
+		data = $( root ).find( '[data-i18]');
 		console.log( data );
 		//Replace all keys with the translated values
 		data.each(function( index, element ) {
@@ -54,17 +54,32 @@ $(document).ready( function() {
 			var el = $( element );
 			key = el.attr( 'data-i18' );
 			if( key ){
+
 				console.log( 'key : ' + key );
 				t =  translation[key];
 				console.log( 'translation : ' + t );
 				console.log( t );
+				//if data 
+				d = el.attr( 'data-i18-data' );
+				if( d ){
+					d = jQuery.parseJSON(d);
+					for (var k in d) {
+					  if (d.hasOwnProperty(k)) {
+					  	console.log( key );
+					  	//replace
+					  	t=t.replace( '%' + k + '%', d[k] );
+					  	console.log( t );
+					    //alert(key + " -> " + p[key]);
+					  }
+					}
+				}
 
 				el.text( t );
 			}
 		});
 
 		//Replace all language key data-i18-lang
-		lng = $( root ).find("[data-i18-lang]");
+		lng = $( root ).find( '[data-i18-lang]' );
 		lng.each(function( index, element ) {
 			var el = $( element );
 			el.text( language );
